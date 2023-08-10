fae6e7e6f1790a253592b974f83734ae27c69932
Mohamed Elhajj <yohajji@MacBook-Pro.local>
2023-08-10 22:00:14 +0200

    yes

---
+++ server/Online_Learning/Online_Learning/Models/Teacher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Online_Learning.Models
+{
+	public class Teacher
+	{
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
+
+
+
+
+
+/* 
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string StateProvince { get; set; }
+        public string PostalCode { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public DateTime JoinDate { get; set; }
+        public string Specialization { get; set; }
+        public string Bio { get; set; }
+ */
